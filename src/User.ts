0205d2ff2d75251e0b2f223d9d25a51b3a371dd1
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-12-20 20:29:31 +0700

    update: class user and integrate with faker

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,16 @@
-class User {}
+import faker from 'faker';
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
